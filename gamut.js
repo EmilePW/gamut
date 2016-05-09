576d726d6574be7122db057faacd5685284b9697
EmilePW <emilepw@hotmail.co.uk>
2016-05-09 17:56:55 +0100

    Refactored to module pattern and added some RGB conversion methods

--- gamut.js
+++ gamut.js
@@ -1,25 +1,6 @@
 'use strict';
 
-var hexLookup = {
-	// List of hexagonal to denary conversion
-	0: 0,
-	1: 1,
-	2: 2,
-	3: 3,
-	4: 4,
-	5: 5,
-	6: 6,
-	7: 7,
-	8: 8,
-	9: 9,
-	A: 10,
-	B: 11,
-	C: 12,
-	D: 13,
-	E: 14,
-	F: 15
-};
-
+// Utility functions
 function partitionStrToArray(str, num) {
 	// Split a string into num-sized paritions
 	str = str.split('');
@@ -39,56 +20,121 @@ function partitionStrToArray(str, num) {
 	return partitionedArray;
 }
 
-function isValidHexadecimalCharacter(char) {
-	// Character needs to be 0-9 or A-F/a-f
-	var code = char.charCodeAt(0);
-	return (48 <= code && code <= 57) || (65 <= code && code <= 70) || (97 <= code && code <= 102);
-}
+// Exported object
+module.exports = {
+	hexToDenary: {
+		// List of hexagonal to denary conversion
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 7,
+		8: 8,
+		9: 9,
+		A: 10,
+		B: 11,
+		C: 12,
+		D: 13,
+		E: 14,
+		F: 15
+	},
 
-function areAllCharactersHexadecimal(str) {
-	str = str.split('');
-	for (var i = 0; i < str.length; i++) {
-		if (!isValidHexadecimalCharacter(str[i])) {
-			return false;
-		} 
-	}
+	denaryToHex: {
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 7,
+		8: 8,
+		9: 9,
+		10: 'A',
+		11: 'B',
+		12: 'C',
+		13: 'D',
+		14: 'E',
+		15: 'F'
+	},
 
-	return true;
-}
+	isValidHexadecimalCharacter: function (char) {
+		// Character needs to be 0-9 or A-F/a-f
+		var code = char.charCodeAt(0);
+		return (48 <= code && code <= 57) || (65 <= code && code <= 70) || (97 <= code && code <= 102);
+	},
 
-function isValidHexadecimal(str) {
-	if (typeof str !== 'string') {
-		return false;
-	}
+	areAllCharactersHexadecimal: function (str) {
+		str = str.split('');
+		for (var i = 0; i < str.length; i++) {
+			if (!this.isValidHexadecimalCharacter(str[i])) {
+				return false;
+			} 
+		}
 
-	// Convert input to standard format of XXXXXX where X is 0-9/A-F/a-f
-	str = str.replace('#', '');
-	if (str.length === 3) {
-		str = str.concat(str);
-	}
-
-	if (str.length !== 6 || !areAllCharactersHexadecimal(str)) {
-		return false;
-	}
-	else {
 		return true;
-	}
-}
+	},
 
-function convertHexToRGB(str) {
-	if (isValidHexadecimal(str)) {
-		var hex = partitionStrToArray(str, 2);
+	isValidHexadecimal: function (str) {
+		if (typeof str !== 'string') {
+			return false;
+		}
 
-		var rgb = hex.map(function(charPair, i) {
-			charPair = charPair.split('');
+		// Convert input to standard format of XXXXXX where X is 0-9/A-F/a-f
+		str = str.replace('#', '');
+		if (str.length === 3) {
+			str = str.concat(str);
+		}
 
-			return hexLookup[charPair[0]] * 16 + hexLookup[charPair[1]];
+		if (str.length !== 6 || !this.areAllCharactersHexadecimal(str)) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	},
+
+	convertHexToRGB: function (str) {
+		var objScope = this;
+		if (this.isValidHexadecimal(str)) {
+			var hex = partitionStrToArray(str, 2);
+
+			var rgb = hex.map(function(charPair, i) {
+				charPair = charPair.split('');
+
+				return objScope.hexLookup[charPair[0]] * 16 + objScope.hexLookup[charPair[1]];
+			});
+
+			return rgb;
+		}
+		else {
+			return false;
+		}
+	},
+
+	isValidRGB: function(arr) {
+		if (arr.length !== 3 || (Number.isInteger(arr[0]) && 0 <= arr[0] && arr[0] <= 255) || (Number.isInteger(arr[1]) && 0 <= arr[1] && arr[1] <= 255) || (Number.isInteger(arr[2]) && 0 <= arr[2] && arr[2] <= 255)) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	},
+
+	convertRGBToHex: function(arr) {
+		if (!this.isValidRGB(arr)) {
+			return false;
+		}
+
+		var objScope = this;
+		arr = arr.map(function(num) {
+			return objScope.denaryToHex[Math.floor(num / 16)].toString() + objScope.denaryToHex[num % 16].toString(); 
 		});
 
-		return rgb;
+		return arr.join('');
 	}
-	else {
-		return false;
-	}
-}
+};
 
